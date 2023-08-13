a96ed41eb52826441ed33ab758214bfd80162762
daniil-torubara <dabarara@bk.ru>
2023-08-13 19:15:21 +0600

    module 4.2 final task

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = cathetusLength; i > 0; i--) {
+            // print spaces
+            for (int j = 2; j <= i; ++j) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= (cathetusLength - i); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
